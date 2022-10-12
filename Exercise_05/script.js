1ae7d2754622084b7653994ce2ee53d6b2bbe0f5
mattangebert <mattan.gebert@flagbit.de>
2022-10-12 13:32:18 +0200

    [EXE_5] feat: Add swan fake ducks

--- Exercise_05/script.js
+++ Exercise_05/script.js
@@ -5,3 +5,8 @@ function getRandomDuckPosition() {
 
 document.querySelector('.duck').style.top = getRandomDuckPosition();
 document.querySelector('.duck').style.left = getRandomDuckPosition();
+
+document.querySelectorAll('.swan').forEach((e) => {
+    e.style.top = getRandomDuckPosition();
+    e.style.left = getRandomDuckPosition();
+})
