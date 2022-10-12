1588453c38a029e483741de6d2928455f8f4cc46
mattangebert <mattan.gebert@flagbit.de>
2022-10-12 11:04:50 +0200

    [EXE_5] chore: Remove debugging

--- Exercise_05/script.js
+++ Exercise_05/script.js
@@ -3,8 +3,5 @@ function getRandomDuckPosition() {
     return Math.floor(Math.random() * (90-5+1)+ 5) + '%';
 }
 
-// debugging
-document.querySelector('.duck').style.opacity = 1;
-
 document.querySelector('.duck').style.top = getRandomDuckPosition();
 document.querySelector('.duck').style.left = getRandomDuckPosition();
