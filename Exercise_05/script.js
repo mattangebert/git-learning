4671570555c5719a4363f135206dbced42742f48
mattangebert <mattan.gebert@flagbit.de>
2022-11-10 13:37:23 +0100

    [EXE_5] Finish Game
    Randomize bubble animation and add new ones if bubble is empty.
    
    Refs: EXE_5

--- Exercise_05/script.js
+++ Exercise_05/script.js
@@ -14,6 +14,19 @@ function createWave(element) {
     element.appendChild(waveWrapper);
 }
 
+function createBubble() {
+    const bubble = document.createElement("div");
+    bubble.classList.add('animal');
+    bubble.style.position = 'absolute';
+    bubble.style.top = getRandomDuckPosition();
+    bubble.style.left = getRandomDuckPosition();
+
+    createWave(bubble);
+
+    const main = document.querySelector("main");
+    main.insertBefore(bubble, main.firstChild);
+}
+
 const duck = document.querySelector('.duck');
 
 duck.style.top = getRandomDuckPosition();
@@ -28,28 +41,39 @@ document.querySelectorAll('.swan').forEach((e) => {
 
 let bubbles = [];
 for (let i = 0; i < 10; i++) {
-    console.log('test');
-
-
-    bubbles[i] = document.createElement("div");
-    bubbles[i].classList.add('animal');
-    bubbles[i].style.position = 'absolute';
-    bubbles[i].style.top = getRandomDuckPosition();
-    bubbles[i].style.left = getRandomDuckPosition();
-
-    createWave(bubbles[i]);
-
-    const main = document.querySelector("main");
-
-    main.insertBefore(bubbles[i], main.firstChild);
+   createBubble();
 }
 
+document.querySelectorAll('.animal').forEach((animal) => {
+    const delay = (Math.random() * (5 + 1)) * 1000;
 
+    for (let i = 0; i < animal.childNodes.length; i++) {
+        if (animal.childNodes[i].classList !== undefined && animal.childNodes[i].classList.contains('wave-wrapper')) {
+            animal.childNodes[i].classList.add('paused');
+
+            setTimeout(() => {
+                animal.childNodes[i].classList.remove('paused');
+            }, delay)
+        }
+    }
+});
 
 document.addEventListener('click', (e) => {
     const animal = e.target.closest('.animal');
 
     if (animal !== null) {
         animal.classList.add('visible');
+
+        if (animal.classList.contains('duck')) {
+            setTimeout(function () {
+                alert('GG you found the duck');
+                location.reload();
+            }, 1000)
+        }
+
+        if (!animal.classList.contains('duck') && !animal.classList.contains('swan')) {
+            createBubble();
+            animal.remove();
+        }
     }
 });
